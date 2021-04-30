ccb6482f59b80407ad5afd4b2aa35a6fd9134007
Victor <victor.fernandez@sage.com>
2021-04-30 14:18:29 +0200

    fourth

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -1,15 +1,11 @@
 ﻿using GradeBook.GradeBooks;
 using System;
 
-namespace GradeBook.UserInterfaces
-{
-    public static class StartingUserInterface
-    {
+namespace GradeBook.UserInterfaces {
+    public static class StartingUserInterface {
         public static bool Quit = false;
-        public static void CommandLoop()
-        {
-            while (!Quit)
-            {
+        public static void CommandLoop() {
+            while (!Quit) {
                 Console.WriteLine(string.Empty);
                 Console.WriteLine(">> What would you like to do?");
                 var command = Console.ReadLine().ToLower();
@@ -17,8 +13,7 @@ namespace GradeBook.UserInterfaces
             }
         }
 
-        public static void CommandRoute(string command)
-        {
+        public static void CommandRoute(string command) {
             if (command.StartsWith("create"))
                 CreateCommand(command);
             else if (command.StartsWith("load"))
@@ -31,25 +26,33 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("{0} was not recognized, please try again.", command);
         }
 
-        public static void CreateCommand(string command)
-        {
+        public static void CreateCommand(string command) {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
-            {
-                Console.WriteLine("Command not valid, Create requires a name.");
+            if (parts.Length != 3) {
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
+
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook = null;
+            if (parts[2] == "standard") {
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if (parts[2] == "ranked") {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else {
+                Console.WriteLine(parts[2] + " is not a supported type of gradebook, please try again");
+                return;
+            }
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
-        public static void LoadCommand(string command)
-        {
+        public static void LoadCommand(string command) {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
-            {
+            if (parts.Length != 2) {
                 Console.WriteLine("Command not valid, Load requires a name.");
                 return;
             }
@@ -62,12 +65,11 @@ namespace GradeBook.UserInterfaces
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
-        public static void HelpCommand()
-        {
+        public static void HelpCommand() {
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
